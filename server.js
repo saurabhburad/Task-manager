221136766d2c3cf08711b64fb276d2e5dfe62dde
Saurabh Burad <saurabhburad@gmail.com>
2020-10-28 20:52:13 +0530

    Update server file

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
